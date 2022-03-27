179e36ee238325781988357892e6a2b8ffef3609
if <yifeichen.chen@hotmail.com>
2022-03-27 13:19:26 +0800

    scratch update

--- codes/scratch/app/src/main/java/com/yifeeeeei/bytheway/MainActivity.java
+++ codes/scratch/app/src/main/java/com/yifeeeeei/bytheway/MainActivity.java
@@ -11,6 +11,7 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,19 +23,42 @@ public class MainActivity extends AppCompatActivity {
 
 
         // create record
-        ParseObject object = new ParseObject("ExampleObject");
-        object.put("name","zhu bing");
-        object.put("age","20");
+//        ParseObject object = new ParseObject("ExampleObject");
+//        object.put("name","zhu bing");
+//        object.put("age","20");
+//
+//        object.saveInBackground(new SaveCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if(e == null){
+//                    Log.d("Parse","save successfully");
+//                }else{
+//                    Log.d("Parse", "save unsuccessfully! " + e.toString());
+//                }
+//            }
+//        });
+        ParseUser user = new ParseUser();
+        user.setUsername("cyf");
+        user.setPassword("my pass");
+        user.setEmail("cyf@ple.com");
 
-        object.saveInBackground(new SaveCallback() {
-            @Override
+// other fields can be set just like with ParseObject
+        user.put("phone", "650-253-0000");
+
+        user.signUpInBackground(new SignUpCallback() {
             public void done(ParseException e) {
-                if(e == null){
-                    Log.d("Parse","save successfully");
-                }else{
-                    Log.d("Parse", "save unsuccessfully! " + e.toString());
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    ParseUser parseUser = ParseUser.getCurrentUser();
+                    Log.d("username",parseUser.getUsername());
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                    Log.d("err","something went wrong");
+                    Log.d("err",e.toString());
                 }
             }
         });
+
     }
 }
